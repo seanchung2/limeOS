5cf9b12917dda5f89950de7731a084e2b0cea183
Lin <angyang2@illinois.edu>
2017-10-22 10:37:56 -0500

    system call test done

--- student-distrib/int_handler.c
+++ student-distrib/int_handler.c
@@ -1,6 +1,13 @@
 #include "int_handler.h"
 #include "lib.h"
 
+/*
+* sys_handler()
+* testing for system call
+* input: none
+* output: none
+* side effect: as description
+*/
 void sys_handler ()
 {
 	printf("System Call !!!!!!");
