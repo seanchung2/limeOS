034f75d7a893a1baf2101c46538b07fd30f64ac5
Chung <chchung2@illinois.edu>
2017-10-29 15:12:59 -0500

    read_dentry_by_name_Test

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -252,69 +252,6 @@ void RTC_test(){
 	puts("RTC TEST COMPLETE");
 }
 
-/* copy_by_index_test
- * 
- * Test the copy dentry by index function
- * Inputs: None
- * Side Effects: None
- * Coverage: filesystem.c
- */
-int copy_by_index_test()  {
-	TEST_HEADER;
-
-	dentry_t test;
-	uint8_t test_index = 17;
-	uint32_t test_type = 0;
-	uint32_t test_inode = 0;
-	int i = 0;
-	uint8_t* char_ptr;
-
-	read_dentry_by_index(test_index, &test);
-	char_ptr = (uint8_t*)&test;
-	test_type = test.file_type;
-	test_inode = test.inode_number;
-	while(*char_ptr != '\0' && i < 32)  {
-		putc(*char_ptr);
-		char_ptr++;
-		i++;
-	}
-	printf("\nType: %d", test_type); 
-	printf("\nINode: %d", test_inode);
-	putc('\n');
-	return 0;
-}
-
-/* copy_by_fname_test
- * 
- * Test the copy dentry by name function
- * Inputs: None
- * Side Effects: None
- * Coverage: filesystem.c
- */
-int copy_by_fname_test()  {
-	TEST_HEADER;
-
-	dentry_t test;
-	uint8_t test_name[34] = "verylargetextwithverylongname.txt\0";
-	uint32_t test_type = 0;
-	uint32_t test_inode = 0;
-	int i = 0;
-	uint8_t* char_ptr;
-
-	read_dentry_by_name(test_name, &test);
-	test_type = test.file_type;
-	test_inode = test.inode_number;
-	char_ptr = (uint8_t*)&test;
-	while(*char_ptr != '\0' && i < 32)  {
-		putc(*char_ptr);
-		char_ptr++;
-		i++;
-	}
-	printf("\nType: %d", test_type);
-	printf("\nINode: %d", test_inode);
-	putc('\n');
-	return 0;
-}
 
 /* read_data_test
  * 
@@ -387,6 +324,7 @@ int open_file_test()  {
  * 
  * Test the function "read_dentry_by_index" by checking the name
  * Inputs: None
+ * Outputs: PASS/FAIL
  * Side Effects: None
  * Coverage: filesystem.c
  */
@@ -441,6 +379,71 @@ int read_dentry_by_index_Test()  {
 	return result;
 }
 
+/* read_dentry_by_name_Test
+ * 
+ * Test the function "read_dentry_by_name" by checking the name
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: None
+ * Coverage: filesystem.c
+ */
+int read_dentry_by_name_Test()  {
+	TEST_HEADER;
+
+	const uint8_t test_num = 17;
+	const uint8_t test_size = 35;
+	const uint8_t max_length = 32;
+	dentry_t test;
+	int result = PASS;
+	int8_t name[test_size];
+	int i = 0;
+	int8_t* name_file[test_num][test_size];
+		name_file[0][0] = ".\0";										//0
+		name_file[1][0] = "sigtest\0";									//1
+		name_file[2][0] = "shell\0";									//2
+		name_file[3][0] = "grep\0";										//3
+		name_file[4][0] = "syserr\0";									//4
+		name_file[5][0] = "rtc\0";										//5
+		name_file[6][0] = "fish\0";										//6
+		name_file[7][0] = "counter\0";									//7
+		name_file[8][0] = "pingpong\0";									//8
+		name_file[9][0] = "cat\0";										//9
+		name_file[10][0] = "frame0.txt\0";								//10
+		name_file[11][0] = "verylargetextwithverylongname.txt\0";		//11
+		name_file[12][0] = "ls\0";										//12
+		name_file[13][0] = "testprint\0";								//13
+		name_file[14][0] = "created.txt\0";								//14
+		name_file[15][0] = "frame1.txt\0";								//15
+		name_file[16][0] = "hello\0";
+
+	for(i=0;i<test_num;i++)
+	{
+		read_dentry_by_name((uint8_t*)name_file[i][0], &test);
+
+		/* copy the name of the current file */
+		strcpy(name, (int8_t*)&test);
+
+		/* check if the name is the same as expectation */
+		if(strlen(name) > max_length)
+		{
+			if(strncmp(name, name_file[i][0], max_length) != 0)
+				result = FAIL;
+		}
+		else
+		{
+			if(strncmp(name, name_file[i][0], strlen(name)) != 0)
+				result = FAIL;
+		}
+	}
+
+	/* test if it will return -1 when the name is not any file we have now */
+	name_file[0][0] = "ece391IsTooExhauting\0";
+	if(read_dentry_by_name((uint8_t*)name_file[0][0], &test) != -1)
+		result = FAIL;
+
+	return result;
+}
+
 /* read_file_test
  * 
  * Test the read_file function
@@ -456,8 +459,8 @@ void read_file_test()  {
 	test_fd = open_file(test_name);
 	read_file(test_fd, test_buf, 10);
 
-	terminal_read((int8_t*)test_buf);
-	terminal_write(0);
+//	terminal_read((int8_t*)test_buf);
+//	terminal_write(0);
 
 	close_file(0);
 }
@@ -502,6 +505,7 @@ void launch_tests(){
 	//read_data_test();
 	//read_directory_test();
 	TEST_OUTPUT("Read Dentry by Index Test", read_dentry_by_index_Test());
+	TEST_OUTPUT("Read Dentry by Name Test", read_dentry_by_name_Test());
 	//TEST_OUTPUT("open_file_test", open_file_test());
 	//read_file_test();
 }
